067f00aff6b5bbcf6ce17638619ff34b5140f07c
miruka <xchrismiruka@gmail.com>
2020-05-07 15:39:44 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Store</title>
+    <link rel="stylesheet" href="bootstrap-4.4.1-dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="fontawesome-free-5.13.0-web/css/fontawesome.min.css">
+    <link rel="stylesheet" href="styles/style.css">
+
+</head>
+
+<body>
+    <div id="top">
+        <!--  Top Begin -->
+        <div class="container">
+
+        </div>
+    </div><!--  Top End -->
+
+
+
+
+
+
+
+
+    <script src="bootstrap-4.4.1-dist/js/jquery-3.5.1.min.js"></script>
+    <script src="bootstrap-4.4.1-dist/js/bootstrap.min.js"></script>
+</body>
+
+</html>
