46e3af6c6f07b0f35e33169f453db6a6b732d567
miruka <xchrismiruka@gmail.com>
2020-05-07 22:11:48 +0300

    updated bootstrap and jquery

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>My Store</title>
-    <link rel="stylesheet" href="bootstrap-4.4.1-dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="bootstrap-3.3.7-dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="fontawesome-free-5.13.0-web/css/fontawesome.min.css">
     <link rel="stylesheet" href="styles/style.css">
 
@@ -15,19 +15,67 @@
     <div id="top">
         <!--  Top Begin -->
         <div class="container">
+            <div class="col-md-6 offer">
+                <a href="#" class="btn btn-success btn-sm">Welcome</a>
+                <a href="checkout.php">4 Items in your cart | Total Price: Ksh 3000 </a>
+            </div>
+            <div class="col-md-6">
+                <ul class="menu">
+                    <li>
+                        <a href="customer_register.php">Register</a>
+                    </li>
+                    <li>
+                        <a href="checkout.php">My Account</a>
+                    </li>
+                    <li>
+                        <a href="cart.php">Go To Cart</a>
+                    </li>
+                    <li>
+                        <a href="checkout.php">Log In</a>
+                    </li>
+                </ul>
+
+            </div>
+
+
 
         </div>
-    </div><!--  Top End -->
+    </div><!-- Top Finish -->
+    <div id="navbar" class="navbar navbar-default">
+        <div class="container">
+            <div class="navbar-header">
+                <a href="index.php" class="navbar-brand home">
+                    <img src="images/mystore logo.png" alt="mystore logo" width="83px" height="53px">
+                </a>
+
+                <button class="navbar-toggle" data-toggle="collapse" data-target="#navigation">
+                    <span class="sr-only">Toggle Navigation</span>
+                    <i class="fa fa-align-justify"></i>
+                </button>
+
+                <button class="navbar-toggle" data-toggle="collapse" data-target="#search">
+                    <span class="sr-only">Toggle Search</span>
+                    <i class="fa fa-search"></i>
+                </button>
+            </div>
+            <div class="navbar-collapse collapse" id="navigation">
+                <div class="padding-nav">
+                    <ul class="nav navbar-nav left">
+                        <li class="active"><a href="index.php">Home</a></li>
+                        <li> <a href="shop.php">Shop</a></li>
+                        <li> <a href="checkout.php">My Account</a></li>
+                        <li> <a href="cart.php">Shopping Cart</a></li>
+                        <li> <a href="content.php">Contact Us</a></li>
+                    </ul>
+                </div>
+
+            </div>
+        </div>
+    </div>
 
 
-
-
-
-
-
-
-    <script src="bootstrap-4.4.1-dist/js/jquery-3.5.1.min.js"></script>
-    <script src="bootstrap-4.4.1-dist/js/bootstrap.min.js"></script>
+    <script src="bootstrap-3.3.7-dist/js/jquery-3.3.1.min.js"></script>
+    <script src="bootstrap-3.3.7-dist/js/bootstrap.min.js"></script>
 </body>
 
 </html>
